5c6e6ead4ef2dbe9d9952a1470220d41275d00da
Hassan Abd El Aziz <hassanabdelaziz940@gmail.com>
2026-07-29 15:40:58 +0300

    update3

--- src/components/HeroSection.vue
+++ src/components/HeroSection.vue
@@ -70,7 +70,7 @@ const imageCount = 199;
 const images = [];
 const imagePaths = Array.from({ length: imageCount }, (_, i) => {
   const frame = String(i + 1).padStart(3, '0');
-  return `imgs/ezgif-frame-${frame}.jpg`;
+  return `public/imgs/ezgif-frame-${frame}.jpg`;
 });
 
 const preloadImages = () => {
